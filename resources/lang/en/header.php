ae62df3d4b45b8428a7ba94d2849e4ce136e60bc
Nemessiz <54395244+Nemessiz@users.noreply.github.com>
2021-06-16 23:23:22 +0300

    Mini update, mostly language

--- resources/lang/en/header.php
+++ resources/lang/en/header.php
@@ -2,4 +2,16 @@
 
 return [
     'Find' => 'Find your opportunity in Latvia',
+
+    'Find2' => 'SIA COMRADE helps people to get jobs and hire employee',
+
+    'Find3' => 'SIA COMRADE offers',
+
+    'Find4' => 'Latest jobs',
+
+    'Find5' => 'Serch by location',
+
+    'Find6' => 'Browse Job Categories',
+
+    'Find7' => 'Browse Top Companies',
 ];
