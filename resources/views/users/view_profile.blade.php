9adcf8bd8611827fbbcf30a64b31fd1dddf24cb3
bitchboi18 <renatoman@mail.ru>
2021-06-17 09:12:43 +0300

    Deleting be my hobbie

--- resources/views/users/view_profile.blade.php
+++ resources/views/users/view_profile.blade.php
@@ -48,8 +48,6 @@
                   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <a class="dropdown-item" href="/users/personalinfo">Personal Information</a>
                     <a class="dropdown-item" href="/users/education">Education</a>
-                    <a class="dropdown-item" href="/users/experience">Experience</a>
-                    <a class="dropdown-item" href="/users/skills">Skills</a>
                   </div>
                 </div>
               </div>
